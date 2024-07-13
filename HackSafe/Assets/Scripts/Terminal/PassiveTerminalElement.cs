58097469b7df0a73d16e9e21a16eb991d8cf4ccb
Kacprucha <karas.kacper@gmail.com>
2024-07-13 17:11:47 +0200

    Fix tworzenia odpowiedzi terminlu

--- HackSafe/Assets/Scripts/Terminal/PassiveTerminalElement.cs
+++ HackSafe/Assets/Scripts/Terminal/PassiveTerminalElement.cs
@@ -6,10 +6,18 @@ using UnityEngine.UI;
 public class PassiveTerminalElement : MonoBehaviour
 {
     [SerializeField] Text textToShow;
+    [SerializeField] RectTransform parentRectTransform;
+
+    static int MaxCharactersInResponse = 44;
+
+    protected float basicHight = 0;
 
     public void UpdateText(string newtext)
     {
         textToShow.text = newtext;
+
+        if (parentRectTransform != null)
+            StartCoroutine (ajustSize ());
     }
 
     public void SetTextToBestFit ()
@@ -19,11 +27,33 @@ public class PassiveTerminalElement : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         
+    }
+
+    protected bool isTextOverflowing (int lengthOfText)
+    {
+        return lengthOfText > MaxCharactersInResponse;
+    }
+
+    IEnumerator ajustSize ()
+    {
+        yield return new WaitForEndOfFrame ();
+
+        int lengthOfResponse = textToShow.text.Length;
+
+        while (isTextOverflowing (lengthOfResponse))
+        {
+            if (basicHight == 0)
+                basicHight = parentRectTransform.sizeDelta.y;
+
+            parentRectTransform.sizeDelta = new Vector2 (parentRectTransform.sizeDelta.x, parentRectTransform.sizeDelta.y + basicHight);
+
+            lengthOfResponse -= MaxCharactersInResponse;
+        }
     }
 }
