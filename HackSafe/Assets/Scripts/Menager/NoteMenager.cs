1e97b6b3de442bfdf43d5c5976039c5d6717666e
Kacprucha <karas.kacper@gmail.com>
2024-11-23 01:32:18 +0100

    Updating note menager

--- HackSafe/Assets/Scripts/Menager/NoteMenager.cs
+++ HackSafe/Assets/Scripts/Menager/NoteMenager.cs
@@ -7,11 +7,14 @@ using UnityEngine.UI;
 public class NoteMenager : MonoBehaviour
 {
     [SerializeField] Text systemNote;
+    [SerializeField] Text linuxNote;
+
+    [SerializeField] int ammountOfLinuxNotes;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        populateLinuxNotes ();
     }
 
     // Update is called once per frame
@@ -33,6 +36,21 @@ public class NoteMenager : MonoBehaviour
             {
                 systemNote.text += localizationManager.GetLocalizedValue (programe.ToString ()) + "\n";
             }
+        }
+    }
+
+    protected void populateLinuxNotes ()
+    {
+        LocalizationManager localizationManager = LocalizationManager.Instance;
+
+        PlayerInfo player = GameState.instance.GetPlayerInfo ();
+        linuxNote.text = "";
+        string baseName = "command";
+
+        for (int i = 0; i < ammountOfLinuxNotes; i++)
+        {
+            string commandName = baseName + i.ToString ();
+            systemNote.text += localizationManager.GetLocalizedValue (commandName) + "\n";
         }
     }
 }
